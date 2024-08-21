3a6f309c2590a6a14041998604575300820d8026
allecosta <alle_costa10@outlook.com>
2024-08-21 18:17:15 -0300

    feat: basic sintaxe challenge

---
+++ src/bank-account/main/TerminalAccount.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.Scanner;
+
+public class TerminalAccount {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numberAccount = scan.nextInt();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String numberAgency = scan.next();
+
+        scan.nextLine();
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nameClient = scan.nextLine();
+
+        System.out.print("Por favor, digite o saldo: ");
+        double balance = scan.nextDouble();
+
+        System.out.println(
+                "\nOlá " + nameClient + ", obrigado por criar uma conta em nosso banco, sua agência é, " + numberAgency
+                        + ", conta " + numberAccount + " e seu saldo " + balance + " já está disponivel para saque.");
+
+        scan.close();
+    }
+}
